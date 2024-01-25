41bc6ad718d848290dc45c9d7184d71611e3ea08
Valentina Piccione <piccione.2030930@studenti.uniroma1.it>
2024-01-25 15:15:15 +0100

    try optional + reconstructed A* path

--- Library/dead_reckoning.py
+++ Library/dead_reckoning.py
@@ -57,3 +57,45 @@ def dead_reckoning(q_real, sigma_d, sigma_theta, L0):
         L_est.append(L_new)
 
     return np.array(q_est), L_est
+
+def dead_reckoning_Astar(q_real, sigma_d, sigma_theta, L0):
+    
+    q_est = [q_real[0]]     # initialize the estimated trajectory
+    L_est = [L0]            # initialize covariance matrix
+
+    # Create measurement Covariance Matrix
+    V = np.array([[sigma_d**2, 0], [0, sigma_theta**2]])
+
+    for i in tqdm(range(1, len(q_real)), desc="Processing"):
+
+        #Dead reckoning
+
+        # Odometry measurements in distance and orientation
+        delta_d = np.linalg.norm(q_real[i][:2] - q_real[i-1][:2])           # delta in distance
+        delta_theta = q_real[i][2] - q_real[i-1][2]                         # delta in orientation
+
+        # Add noise
+        noisy_delta_d = delta_d + np.random.normal(0, sigma_d)
+        noisy_delta_theta = delta_theta + np.random.normal(0, sigma_theta)
+
+        # Retrieve last State and Covariance matrix
+        x, y, theta = q_est[-1]
+        L = L_est[-1]
+
+        # Estimate the new State
+        x_new = x + noisy_delta_d*np.cos(theta)
+        y_new = y + noisy_delta_d*np.sin(theta)
+        theta_new = theta + noisy_delta_theta
+        q_new = np.array([x_new, y_new, theta_new])
+
+        # Estimate the new Covariance Matrix
+        Fq = np.array([[1, 0, -noisy_delta_d*np.sin(theta)], [0, 1, noisy_delta_d*np.cos(theta)], [0, 0, 1]])   # Jacobian of the State
+        Fv = np.array([[np.cos(theta), 0], [np.sin(theta), 0], [0, 1]])   # Jacobian of the noise
+        L_new = Fq @ L @ Fq.T + Fv @ V @ Fv.T   # Covariance Matrix of the new State (@ represents matrix multiplication)
+                
+
+        # Append the new estimated position and orientation
+        q_est.append(q_new)
+        L_est.append(L_new)
+
+    return np.array(q_est), L_est
--- Library/get_indices.py
+++ Library/get_indices.py
@@ -8,3 +8,11 @@ def get_indices(point, X, Y, mapRes):
     i = int((Y[0, 0] - point[1] + mapRes) / mapRes) - 1   # row index
 
     return np.array([i, j])
+
+def get_point(indices, X, Y, mapRes):
+    # Compute point
+    x = X[0, 0] + (indices[1] + 1) * mapRes - mapRes
+    y = Y[0, 0] - (indices[0] + 1) * mapRes + mapRes
+
+    return np.array([x, y])
+
