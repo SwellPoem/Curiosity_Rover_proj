0c0d14a035d336c8737bf8c222b3fbdf19ecd621
Valentina Piccione <piccione.2030930@studenti.uniroma1.it>
2024-01-28 18:48:22 +0100

    changing in plotting ellipses

--- Library/EKF.py
+++ Library/EKF.py
@@ -1,22 +1,24 @@
 import numpy as np
+import matplotlib.cm as cm
 from tqdm import tqdm
-import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 
-def plot_covariance_ellipse(x, P, n_sigma=1, facecolor='none', **kwargs):
-    P = P[0:2, 0:2]
-    if not(np.all(np.isfinite(P))):
-        return
-
-    U, s, _ = np.linalg.svd(P)
-    orientation = np.degrees(np.arctan2(U[1, 0], U[0, 0]))
-    width = n_sigma * np.sqrt(s[0])
-    height = n_sigma * np.sqrt(s[1])
-
-    ellipse = Ellipse(xy=x, width=width, height=height, angle=orientation,
-                      facecolor=facecolor, **kwargs)
-
-    plt.gca().add_patch(ellipse)
+def compute_and_plot_ellipses(q_est, L_est, plt, n_sigma=3, frequency=1):
+    n = int(1 / frequency)  # number of measurements acquisition per second
+    cmap = cm.get_cmap('Reds')  # load the colormap
+    num_ellipses = len(q_est) // n  # calculate the number of ellipses
+    for i in range(0, len(q_est), n):
+        # Compute the ellipse of uncertainty
+        L = L_est[i]
+        s, U = np.linalg.eig(L[0:2, 0:2])
+        orientation = np.degrees(np.arctan2(U[1, 0], U[0, 0]))
+        width = np.sqrt(s[0])
+        height = np.sqrt(s[1])
+        # Get a color from the colormap
+        color = cmap(i / num_ellipses)
+        # Plot the ellipse
+        ellipse = Ellipse(xy=q_est[i, :2], width=width*n_sigma, height=height*n_sigma, angle=orientation, facecolor='none', edgecolor=color, alpha=0.8)
+        plt.gca().add_patch(ellipse)
 
 def EKF(q_real, sigma_d, sigma_theta, L0, lidar_maxrange, sigma_r, sigma_beta, xLM = [], yLM= []):
     
@@ -38,8 +40,8 @@ def EKF(q_real, sigma_d, sigma_theta, L0, lidar_maxrange, sigma_r, sigma_beta, x
         delta_theta = q_real[i][2] - q_real[i-1][2]                         # delta in orientation
 
         # Add noise
-        noisy_delta_d = delta_d + np.random.normal(0, sigma_d)
-        noisy_delta_theta = delta_theta + np.random.normal(0, sigma_theta)
+        noisy_delta_d = delta_d + np.random.normal(loc=0, scale=sigma_d)
+        noisy_delta_theta = delta_theta + np.random.normal(loc=0, scale=sigma_theta)
 
         # Retrieve last State and Covariance matrix
         x, y, theta = q_est[-1]
@@ -52,7 +54,7 @@ def EKF(q_real, sigma_d, sigma_theta, L0, lidar_maxrange, sigma_r, sigma_beta, x
         q_new = np.array([x_new, y_new, theta_new])
 
         # Estimate the new Covariance Matrix
-        Fq = np.array([[1, 0, -noisy_delta_d*np.sin(theta)], [0, 1, noisy_delta_d*np.cos(theta)], [0, 0, 1]])   # Jacobian of the State
+        Fq = np.array([[1, 0, -delta_d*np.sin(theta)], [0, 1, delta_d*np.cos(theta)], [0, 0, 1]])   # Jacobian of the State
         Fv = np.array([[np.cos(theta), 0], [np.sin(theta), 0], [0, 1]])   # Jacobian of the noise
         L_new = Fq @ L @ Fq.T + Fv @ V @ Fv.T   # Covariance Matrix of the new State (@ represents matrix multiplication)
 
@@ -66,7 +68,7 @@ def EKF(q_real, sigma_d, sigma_theta, L0, lidar_maxrange, sigma_r, sigma_beta, x
               
                 # Compute the Observables from the real State -> observables that I should see
                 # z_real = h(q_real[i], x_lm, y_lm) + np.array([np.random.normal(0, sigma_r), np.random.normal(0, sigma_beta)])
-                z_real = h(q_real[i], x_lm, y_lm) + np.array([np.random.normal(0, sigma_r), np.random.normal(0, sigma_beta)])
+                z_real = h(q_real[i], x_lm, y_lm) + np.array([np.random.normal(loc=0, scale=sigma_r), np.random.normal(loc=0, scale=sigma_beta)])
                 z_new = h(q_new, x_lm, y_lm)
 
                 # Compute Kalman Gain
--- Library/dead_reckoning.py
+++ Library/dead_reckoning.py
@@ -1,20 +1,7 @@
+import matplotlib.cm as cm
 import numpy as np
 from tqdm import tqdm
 from matplotlib.patches import Ellipse
-
-def plot_covariance_ellipse(x, P, plt, n_sigma=1, facecolor='none', **kwargs):
-    P = P[0:2, 0:2]
-    if not(np.all(np.isfinite(P))):
-        return
-
-    U, s, _ = np.linalg.svd(P)
-    orientation = np.degrees(np.arctan2(U[1, 0], U[0, 0]))
-    width = n_sigma * np.sqrt(s[0])
-    height = n_sigma * np.sqrt(s[1])
-
-    ellipse = Ellipse(xy=x, width=width, height=height, angle=orientation, facecolor=facecolor, **kwargs)
-
-    plt.gca().add_patch(ellipse)
 
 def dead_reckoning(q_real, sigma_d, sigma_theta, L0):
     
@@ -33,8 +20,8 @@ def dead_reckoning(q_real, sigma_d, sigma_theta, L0):
         delta_theta = q_real[i][2] - q_real[i-1][2]                         # delta in orientation
 
         # Add noise
-        noisy_delta_d = delta_d + np.random.normal(0, sigma_d)
-        noisy_delta_theta = delta_theta + np.random.normal(0, sigma_theta)
+        noisy_delta_d = delta_d + np.random.normal(loc=0, scale=sigma_d)
+        noisy_delta_theta = delta_theta + np.random.normal(loc=0, scale=sigma_theta)
 
         # Retrieve last State and Covariance matrix
         x, y, theta = q_est[-1]
@@ -47,10 +34,9 @@ def dead_reckoning(q_real, sigma_d, sigma_theta, L0):
         q_new = np.array([x_new, y_new, theta_new])
 
         # Estimate the new Covariance Matrix
-        Fq = np.array([[1, 0, -noisy_delta_d*np.sin(theta)], [0, 1, noisy_delta_d*np.cos(theta)], [0, 0, 1]])   # Jacobian of the State
+        Fq = np.array([[1, 0, -delta_d*np.sin(theta)], [0, 1, delta_d*np.cos(theta)], [0, 0, 1]])   # Jacobian of the State
         Fv = np.array([[np.cos(theta), 0], [np.sin(theta), 0], [0, 1]])   # Jacobian of the noise
         L_new = Fq @ L @ Fq.T + Fv @ V @ Fv.T   # Covariance Matrix of the new State (@ represents matrix multiplication)
-                
 
         # Append the new estimated position and orientation
         q_est.append(q_new)
@@ -58,44 +44,20 @@ def dead_reckoning(q_real, sigma_d, sigma_theta, L0):
 
     return np.array(q_est), L_est
 
-def dead_reckoning_Astar(q_real, sigma_d, sigma_theta, L0):
-    
-    q_est = [q_real[0]]     # initialize the estimated trajectory
-    L_est = [L0]            # initialize covariance matrix
+def compute_and_plot_ellipses(q_est, L_est, plt, n_sigma=3, frequency=1):
+    n = int(1 / frequency)  # number of measurements acquisition per second
+    cmap = cm.get_cmap('Reds')  # load the colormap
+    num_ellipses = len(q_est) // n  # calculate the number of ellipses
 
-    # Create measurement Covariance Matrix
-    V = np.array([[sigma_d**2, 0], [0, sigma_theta**2]])
-
-    for i in tqdm(range(1, len(q_real)), desc="Processing"):
-
-        #Dead reckoning
-
-        # Odometry measurements in distance and orientation
-        delta_d = np.linalg.norm(q_real[i][:2] - q_real[i-1][:2])           # delta in distance
-        delta_theta = q_real[i][2] - q_real[i-1][2]                         # delta in orientation
-
-        # Add noise
-        noisy_delta_d = delta_d + np.random.normal(0, sigma_d)
-        noisy_delta_theta = delta_theta + np.random.normal(0, sigma_theta)
-
-        # Retrieve last State and Covariance matrix
-        x, y, theta = q_est[-1]
-        L = L_est[-1]
-
-        # Estimate the new State
-        x_new = x + noisy_delta_d*np.cos(theta)
-        y_new = y + noisy_delta_d*np.sin(theta)
-        theta_new = theta + noisy_delta_theta
-        q_new = np.array([x_new, y_new, theta_new])
-
-        # Estimate the new Covariance Matrix
-        Fq = np.array([[1, 0, -noisy_delta_d*np.sin(theta)], [0, 1, noisy_delta_d*np.cos(theta)], [0, 0, 1]])   # Jacobian of the State
-        Fv = np.array([[np.cos(theta), 0], [np.sin(theta), 0], [0, 1]])   # Jacobian of the noise
-        L_new = Fq @ L @ Fq.T + Fv @ V @ Fv.T   # Covariance Matrix of the new State (@ represents matrix multiplication)
-                
-
-        # Append the new estimated position and orientation
-        q_est.append(q_new)
-        L_est.append(L_new)
-
-    return np.array(q_est), L_est
+    for i in range(0, len(q_est), n):
+        # Compute ellipse of uncertainty
+        L = L_est[i]
+        s, U = np.linalg.eig(L[0:2, 0:2])
+        orientation = np.degrees(np.arctan2(U[1, 0], U[0, 0]))
+        width = np.sqrt(s[0])
+        height = np.sqrt(s[1])
+        # Get a color from the colormap
+        color = cmap(i / num_ellipses)
+        # Plot the ellipse
+        ellipse = Ellipse(xy=q_est[i, :2], width=width*n_sigma, height=height*n_sigma, angle=orientation, facecolor='none', edgecolor=color, alpha=0.9)
+        plt.gca().add_patch(ellipse)
