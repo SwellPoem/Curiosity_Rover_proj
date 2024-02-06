008f9f654e98b77190489c2b687dddef0c61ba94
Valentina Piccione <piccione.2030930@studenti.uniroma1.it>
2024-02-06 17:29:36 +0100

    last modifications

--- Library/kinematicModel.py
+++ Library/kinematicModel.py
@@ -1,8 +1,7 @@
 import numpy as np
 from Library.move2pose import move2pose
 
-#K=[1e-2, 2, -10]
-def kinematicModel(t, S, K, v_values, omega_values, alpha_values):
+def kinematicModel(t, S, K):
     '''
     S contains the polar state variables [rho, alpha, beta].
     '''
@@ -15,11 +14,6 @@ def kinematicModel(t, S, K, v_values, omega_values, alpha_values):
     # Compute Control
     v, omega = move2pose(S, K)
 
-    # Store v and omega in the global variables
-    v_values.append(v)
-    omega_values.append(omega)
-    alpha_values.append(alpha)
-
     # Compute Derivatives
     drho = -np.cos(alpha) * v
     dalpha = np.sin(alpha) * v / rho - omega
@@ -27,4 +21,4 @@ def kinematicModel(t, S, K, v_values, omega_values, alpha_values):
 
     dS = np.array([drho, dalpha, dbeta])
 
-    return dS, v_values, omega_values, alpha_values
+    return dS, v, omega
